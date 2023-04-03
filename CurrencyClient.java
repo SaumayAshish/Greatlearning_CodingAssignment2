80c6e15ef8421c0694fe94607839774d0cd7cbcf
SaumayAshish <124284375+SaumayAshish@users.noreply.github.com>
2023-04-03 18:34:45 +0530

    Add files via upload

---
+++ CurrencyClient.java
@@ -0,0 +1,14 @@
+package com.glearning.client;
+
+import com.glearning.service.CurrencyService;
+
+public class CurrencyClient {
+	
+	public static void main(String[] args) {
+		CurrencyService service = new CurrencyService();
+		int[] denominators = new int[] {500, 200, 100, 50, 20, 10};
+		service.denominatorService(denominators, 3650);
+	}
+
+}
+
